79f3d183c35420825be3fa3235465af4003cc4ba
Vishnu V V <ng.pingme@gmail.com>
2022-09-29 18:54:35 +0100

    Add decorator for JobDto

--- src/jobs/dto/create-job.dto.ts
+++ src/jobs/dto/create-job.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateJobDto {
+    @ApiProperty()
     title: string;
+
+    @ApiProperty()
     description: string;
+
+    @ApiProperty()
     active: boolean;
 }
