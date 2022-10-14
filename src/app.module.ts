ac0aaead12ffd6f4f5d3a5a7d95c3f6eaba2239c
Vishnu V V <ng.pingme@gmail.com>
2022-10-14 18:15:04 +0100

    Add AdminBro admin panel framework

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+import * as AdminJSSequelize from '@adminjs/sequelize'
+import AdminJS from 'adminjs'
+import { AdminModule } from '@adminjs/nestjs'
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
@@ -9,8 +14,46 @@ import { PassionsModule } from './passions/passions.module';
 import { SkillsModule } from './skills/skills.module';
 import { UniversitiesModule } from './universities/universities.module';
 
+import { Job } from "./jobs/job.entity";
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN)
+  }
+  return null
+}
+
+AdminJS.registerAdapter({
+  Resource: AdminJSSequelize.Resource,
+  Database: AdminJSSequelize.Database,
+});
+
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true,}),
+  imports: [
+    AdminModule.createAdminAsync({
+      useFactory: () => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [Job],
+        },
+        auth: {
+          authenticate,
+          cookieName: 'adminjs',
+          cookiePassword: 'secret'
+        },
+        sessionOptions: {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret'
+        },
+      }),
+    }),
+    ConfigModule.forRoot({isGlobal: true,}),
     JobsModule,
     PassionsModule,
     PassionCategoriesModule,
